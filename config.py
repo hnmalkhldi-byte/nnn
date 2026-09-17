a46e139bcd91b2919382c77adce16d91833fabb8
hnmalkhldi-byte <hnmalkhldi@gmail.com>
2026-09-17 13:17:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "ضع_التوكن_هنا")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+CHANNEL_LINK = "https://t.me/YourChannel"
+GROUP_LINK = "https://t.me/YourGroup"
+DB_PATH = "novels_bot.db"
